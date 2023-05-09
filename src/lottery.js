545943fb2a15c821039ba061b4ab36ec7ada4608
Douglas Souza <douglascpsouza@gmail.com>
2023-05-09 02:15:55 -0300

    Set input so user can enter the lottery

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0xe01081c27Cd5bb6b420A7303E12FE01a102DB4Cc';
+const abi = [{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }];
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new web3.eth.Contract(abi, address);
